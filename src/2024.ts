61899317bb460cc2e57ce754e5251b318682a337
Sonny <sonny0dev@gmail.com>
2024-12-08 12:38:28 +0000

    chore: day3

--- src/2024.ts
+++ src/2024.ts
@@ -1,7 +1,10 @@
-/* eslint-disable unused-imports/no-unused-vars */
+/* eslint-disable unused-imports/no-unused-vars, antfu/top-level-function */
 
 // Day 1 - Primitives
 type Demand1 = number
 
 // Day 2 - Literals
 type Demand2 = 900_000
+
+// Day 3 - Type function parameter
+const survivalRatio1 = (input: number) => { /* blah */ }
